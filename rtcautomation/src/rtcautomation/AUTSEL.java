d5abd7b2f0220fb80c26902845a331535d83e016
Bhargavi21 <n.bhargavi2424@gmail.com>
2021-07-26 18:44:10 +0530

    first commit

--- rtcautomation/src/rtcautomation/AUTSEL.java
+++ rtcautomation/src/rtcautomation/AUTSEL.java
@@ -1,5 +1,8 @@
 package rtcautomation;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,8 +12,12 @@ public class AUTSEL {
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","C:\\Users\\Dhanunjaya\\Desktop\\EY file\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.google.com");
-
+		driver.get("http://accounts.google.com");
+		driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+		driver.findElement(By.id("identifierId")).sendKeys("n.bhargavi2424@gmail.com");
+		driver.findElement(By.xpath("//*[@id=\'identifierNext\']/div/button/span")).click();
+        driver.findElement(By.name("password")).sendKeys("Bhargavi@Badri21");
+        driver.findElement(By.xpath("//*[@id=\'passwordNext\']/div/button/span")).click();
 	}
 
 }
